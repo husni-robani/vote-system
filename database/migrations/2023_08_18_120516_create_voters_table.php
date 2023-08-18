644232c791eef0ed13f0e25ff786ba38c5e89e8e
husnirobani <husnir2005@gmail.com>
2023-08-18 10:25:14 +0700

    Create migrations for elections, candidates, voters, vote_links table

---
+++ database/migrations/2023_08_18_120516_create_voters_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('voters', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('email');
+            $table->string('name');
+            $table->string('npm');
+            $table->year('gen');
+            $table->foreignUuid('election_id')->constrained()->cascadeOnDelete();
+            $table->foreignUuid('candidate_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('voters');
+    }
+};
