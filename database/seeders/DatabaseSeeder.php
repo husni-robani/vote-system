9a9dd9fa3d3f92f8103accb50ae76e7ae8a13715
husni-robani <husnir2005@gmail.com>
2024-09-05 20:34:17 +0700

    Update default image seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,11 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Candidate;
-use App\Models\Election;
-use App\Models\Generation;
 use App\Models\User;
 use App\Services\ElectionService;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\File;
 use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
@@ -19,55 +18,74 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $election = ElectionService::create([
-            'title' => 'Pemilihan Ketua Himpunan 2022/2023',
+            'title' => 'Testing Vote',
+            'active' => false
+        ]);
+        $election->generations()->create([
+            'year' => 2021
+        ]);
+
+        $election = ElectionService::create([
+            'title' => 'Testing Vote Active',
             'active' => true
         ]);
         $generation = $election->generations()->create([
             'year' => 2020
         ]);
+
+        $source = public_path('default.jpg');
+        $path = "";
+        if (file_exists($source)){
+            $path = Storage::putFile('public/photo/candidates', new File($source));
+            $this->command->info('Image default.jpg copied to storage.');
+        }else {
+            $this->command->warn('Source file default.jpg not found in public directory.');
+        }
+
+
         $election->candidates()->saveMany([
            new Candidate([
-               'name' => 'Husni Robani',
+               'name' => fake()->name,
                'vision' => fake()->text(10),
                'mission' => fake()->text(10),
                'number' => 1,
-               'photo' => 'public/photo/candidates/default.png'
+               'photo' => $path
            ]),
             new Candidate([
-                'name' => 'Rijal Fauzi Iskandar',
+                'name' => fake()->name,
                 'vision' => fake()->text(10),
                 'mission' => fake()->text(10),
                 'number' => 2,
-                'photo' => 'public/photo/candidates/default.png'
+                'photo' => $path
             ]),
             new Candidate([
-                'name' => 'Zidan',
+                'name' => fake()->name,
                 'vision' => fake()->text(10),
                 'mission' => fake()->text(10),
                 'number' => 3,
-                'photo' => 'public/photo/candidates/default.png'
+                'photo' => $path
             ])
         ]);
 
         Candidate::first()->voters()->create([
-            'name' => 'Alif Nuryana',
-            'email' => 'nuryana.alif@widyatama.ac.id',
+            'name' => fake()->name,
+            'email' => fake()->email,
             'npm' => '13242132',
             'generation_id' => $generation->id,
             'election_id' => $election->id
         ]);
 
         Candidate::first()->voters()->create([
-            'name' => 'test',
-            'email' => 'test.@widyatama.ac.id',
+            'name' => fake()->name,
+            'email' => fake()->email,
             'npm' => '13242132',
             'generation_id' => $generation->id,
             'election_id' => $election->id
         ]);
 
         Candidate::first()->voters()->create([
-            'name' => 'test2',
-            'email' => 'test2@widyatama.ac.id',
+            'name' => fake()->name,
+            'email' => fake()->email,
             'npm' => '13242132',
             'generation_id' => $generation->id,
             'election_id' => $election->id
@@ -76,8 +94,8 @@ class DatabaseSeeder extends Seeder
         Candidate::first()->update(['counter' => 3]);
 
          User::create([
-             'name' => 'HIMATIF',
-             'email' => 'hima.teknikinformatika@widyatama.ac.id',
+             'name' => 'Admin',
+             'email' => 'admin@example.com',
              'password' => 'password'
          ]);
     }
